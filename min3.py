570317bdea566034165a3d6c5c178e5fa3a4a809
Aparna-Gopi <aparna.gopi56@gmail.com>
2017-02-01 22:24:42 +0530

    second commit

--- min3.py
+++ min3.py
@@ -4,10 +4,13 @@ print "input data"
 no_val = input()
 for indx in range(0,no_val):
     array_inp = map(int,raw_input().split())
-    if array_inp[0] < array_inp[1]:
+    if array_inp[0] < array_inp[1] and array_inp[0] < array_inp[2]:
         array_min.append(array_inp[0])
-    else:
+    elif array_inp[1] < array_inp[2]:
         array_min.append(array_inp[1])
+    else :
+        array_min.append(array_inp[2])
+
 
 length_arr = len(array_min)
 for indx in range(0,length_arr):
