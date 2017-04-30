698dc9cc3939d5f5c10040d0d449dc5415557d1a
Aparna Gopinath <aparna.gopi56@gmail.com>
2017-04-30 18:52:42 +0530

    Delete max_array.py

--- max_array.py
+++
@@ -1,9 +0,0 @@
-min, max = 0, 0
-array_inp = map(int,raw_input().split())
-length_arr = len(array_inp)
-for indx in range(0,length_arr):
-    if min>array_inp[indx]:
-        min = array_inp[indx]
-    if max<array_inp[indx]:
-        max = array_inp[indx]
-print max, min
